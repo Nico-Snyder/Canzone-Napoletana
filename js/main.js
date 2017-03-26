1ce792075ae8c73c8841f62b5e8032d53d07c061
Nico <nicouly@gmail.com>
2017-03-26 10:55:36 -0400

    prep
    cleaned up loose ends

--- js/main.js
+++ js/main.js
@@ -6,25 +6,33 @@ $(function(){
     event.preventDefault();
     var verb = $('#verbname').val();
     var tense = $('#tenseselecta').val();
+    $('#Translation-Definition').empty();
+    $('.results').empty();
 
-    if(verb === "go" || verb === "to go"){
-      $('#Translation-Definition').append('<footer class="footer">In Neapolitan the verb "to go" can be pronounced three different ways: "ji", "ghi", or "annare".<p>The song <a class=links href="https://www.youtube.com/watch?v=XW0W7j04iRQ" target="_blank">Funiculi, Funicula</a> has a great example of the present tense "we" or "nuje" prounciation of the verb ji.</p> <p>Listen at 1:33 in the link provided.</p></footer>');
+    if(verb === "go"){
+      $('#Translation-Definition').append('<footer class="footer">In Neapolitan the infinitive for the verb "to go" can be pronounced three different ways: "ji", "ghi", or "annare".<p>The song <a class=links href="https://www.youtube.com/watch?v=XW0W7j04iRQ" target="_blank">Funiculi, Funicula</a> has a great example of the present tense "we" or "nuje" prounciation of the verb ji.</p> <p>Listen at 1:33 in the link provided.</p></footer>');
     } if(verb === "go" && tense === "present"){
-      $('#verbbox').append('<div class="conjbox"><ul><li>io</li><li>tu</li><li>isso/essa</li><li>nuje</li><li>vuje</li><li>isse/loro</li></ul><ul class="boxtwo"><li>vaco</li><li>vaje</li><li>va</li><li>jammo</li><li>jate</li><li>vanno</li></ul></div></div>');
-      $('#resetbutton').append('<input type="submit" value="Reset" id="resetactivate">');
+      $('.results').append('<div class="conjbox"><ul><li>io</li><li>tu</li><li>isso/essa</li><li>nuje</li><li>vuje</li><li>isse/loro</li></ul><ul class="boxtwo"><li>vaco</li><li>vaje</li><li>va</li><li>jammo</li><li>jate</li><li>vanno</li></ul></div></div>');
     } if(verb === "go" && tense === "imperfect"){
-      $('#verbbox').append('<div class="conjbox"><ul><li>io</li><li>tu</li><li>isso/essa</li><li>nuje</li><li>vuje</li><li>isse/loro</li></ul><ul class="boxtwo"><li>jevo</li><li>jive</li><li>jeva</li><li>jevemo</li><li>jiveve</li><li>jeveno</li></ul></div></div>');
-      $('#resetbutton').append('<input type="submit" value="Reset" id="resetactivate">');
+      $('.results').append('<div class="conjbox"><ul><li>io</li><li>tu</li><li>isso/essa</li><li>nuje</li><li>vuje</li><li>isse/loro</li></ul><ul class="boxtwo"><li>jevo</li><li>jive</li><li>jeva</li><li>jevemo</li><li>jiveve</li><li>jeveno</li></ul></div></div>');
+    } if(verb === "have" && tense === "present") {
+      $('.results').append('<div class="conjbox"><ul><li>io</li><li>tu</li><li>isso/essa</li><li>nuje</li><li>vuje</li><li>isse/loro</li></ul><ul class="boxtwo"><li>aggio</li><li>he</li><li>ha</li><li>avimo</li><li>avite</li><li>aveno</li></ul></div></div>');
+    } if(verb === "be" && tense === "present") {
+      $('.results').append('<div class="conjbox"><ul><li>io</li><li>tu</li><li>isso/essa</li><li>nuje</li><li>vuje</li><li>isse/loro</li></ul><ul class="boxtwo"><li>songo (so)</li><li>si</li><li>e</li><li>simmo</li><li>site</li><li>songo (so)</li></ul></div></div>');
+    } if (verb === "run" && tense === "present") {
+      $('.results').append('<div class="conjbox"><ul><li>io</li><li>tu</li><li>isso/essa</li><li>nuje</li><li>vuje</li><li>isse/loro</li></ul><ul class="boxtwo"><li>corro</li><li>curre</li><li>corre</li><li>currimmo</li><li>currite</li><li>correne</li></ul></div></div>');
     }
+
   }
 
-  $('#resetactivate').submit(reset);
 
-  function reset(event) {
-    event.preventDefault();
-    console.log('you did it')
-  }
-
+  $('.results').hover(
+    function() {
+      $('.lowmiddle').append($("<div>The Neapolitan infinitive of the verb <span>BLANK</span> is BLANK</div>"));
+    }, function() {
+      $('.lowmiddle').find("div:last").remove();
+    }
+  );
 
 
 });
