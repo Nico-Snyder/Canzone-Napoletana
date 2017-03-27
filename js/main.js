345ac5b30daa4fadf7f7f7fe48bf202c9d963666
Nico <nicouly@gmail.com>
2017-03-27 01:14:35 -0400

    background colors

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ $(function(){
     var tense = $('#tenseselecta').val();
     $('#Translation-Definition').empty();
     $('.results').empty();
+    $('.results').css('background-color: red;')
 
     if(verb === "go"){
       $('#Translation-Definition').append('<footer class="footer">In Neapolitan the infinitive for the verb "to go" can be pronounced three different ways: "ji", "ghi", or "annare".<p>The song <a class=links href="https://www.youtube.com/watch?v=XW0W7j04iRQ" target="_blank">Funiculi, Funicula</a> has a great example of the present tense "we" or "nuje" prounciation of the verb ji.</p> <p>Listen at 1:33 in the link provided.</p></footer>');
@@ -25,14 +26,23 @@ $(function(){
 
   }
 
+  // $('.results').hover(
+  //   function() {
+  //     $('.boxtwo li').css('font-family: 'Ultra', serif;');
+  //   // }, function() {
+  //   //   $('.lowmiddle').find("div:last").remove();
+  //   }
+  // );
 
-  $('.results').hover(
-    function() {
-      $('.lowmiddle').append($("<div>The Neapolitan infinitive of the verb <span>BLANK</span> is BLANK</div>"));
-    }, function() {
-      $('.lowmiddle').find("div:last").remove();
-    }
-  );
+
+
+  // $('.results').hover(
+  //   function() {
+  //     $('.lowmiddle').append($("<div>The Neapolitan infinitive of the verb <span>BLANK</span> is BLANK</div>"));
+  //   }, function() {
+  //     $('.lowmiddle').find("div:last").remove();
+  //   }
+  // );
 
 
 });
